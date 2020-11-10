74ce27ba2935125adb24612b6935b8ab58d8099d
unknown <anhthu127at@gmail.com>
2020-11-10 22:42:40 +0700

    111020

--- src/components/teacher/ListTeachers.js
+++ src/components/teacher/ListTeachers.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { FaBars } from 'react-icons/fa';
 import { withRouter } from 'react-router-dom';
 import { Table } from 'react-table';
+import NavbarForm from '../NavbarForm';
 import Navbars from '../Navbars';
 export default class ListTeachers extends Component {
 
@@ -11,10 +13,16 @@ export default class ListTeachers extends Component {
     render() {
         return (
             <div>
-              <div style={{float:"left"}}>  <Navbars /></div>
+                <div style={{ float: "left" }}>  <Navbars /></div>
                 <div>
+                    <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                        <FaBars style={{ width: "20px" }}
+                            onClick={() => this.onSetSidebarOpen(true)} />
+                        <NavbarForm />
+
+                    </nav>
                     aaaaaaaaaaaaaaaaaa
-                    
+
                     </div>
                 {/* <Table striped bordered hover>
                     <thead>
